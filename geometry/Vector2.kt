fd3f97f8407faca1754c8fceb3cbf96be82e65c6
Ivar Fatland <ivar.fatland@lyse.net>
2022-05-08 01:18:05 +0200

    Moved around some methods

--- geometry/Vector2.kt
+++ geometry/Vector2.kt
@@ -1,6 +1,6 @@
 package king_game_engine.geometry
 
-import king_game_engine.performRevert
+import king_game_engine.swing_extensions.performRevert
 import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.Graphics2D
@@ -32,6 +32,64 @@ class Vector2(x: Number, y: Number) {
     fun round() = Vector2(round(x), round(y))
     fun ceil() = Vector2(ceil(x), ceil(y))
     fun floor() = Vector2(floor(x), floor(y))
+
+    private constructor(both: Number) : this(both, both)
+
+    constructor() : this(0)
+    override fun toString() = "[$x, $y]"
+
+    operator fun plus(other: Vector2) = Vector2(x + other.x, y + other.y)
+
+    operator fun plus(other: Number) = plus(Vector2(other))
+    operator fun minus(other: Vector2) = Vector2(x - other.x, y - other.y)
+
+    operator fun minus(other: Number) = minus(Vector2(other))
+    operator fun times(other: Vector2) = Vector2(x * other.x, y * other.y)
+
+    operator fun times(other: Number) = times(Vector2(other))
+    operator fun div(other: Vector2) = Vector2(x / other.x, y / other.y)
+
+    operator fun div(other: Number) = div(Vector2(other))
+    operator fun unaryPlus() = this
+
+    operator fun unaryMinus() = this * -1
+    operator fun inc() = this + 1
+
+    operator fun dec() = this - 1
+    operator fun rem(other: Vector2) = Vector2(x % other.x, y % other.y)
+
+    operator fun rem(other: Number) = rem(Vector2(other))
+    fun lenSquared() = x * x + y * y
+
+    fun len() = sqrt(lenSquared())
+    infix fun longerThan(other: Vector2) = lenSquared() > other.lenSquared()
+
+    infix fun longerThan(other: Number) = len() > other.toDouble()
+    infix fun shorterThan(other: Vector2) = lenSquared() < other.lenSquared()
+    infix fun shorterThan(other: Number) = len() < other.toDouble()
+    fun dot(other: Vector2) = x * other.x + y * other.y
+
+
+    fun angle() = atan2(y, x)
+    fun angleTo(other: Vector2) = acos(dot(other) / len() * other.len())
+    fun angleToPoint(other: Vector2) = (other - this).angle()
+    fun normalized() = this / len()
+    fun rotatedBy(radians: Double) = fromAngle(angle() + radians, len())
+    fun rotatedAt(radians: Double) = fromAngle(radians, len())
+    override operator fun equals(other: Any?): Boolean {
+        if (other is Vector2) {
+            return (x == other.x) and (y == other.y)
+        }
+
+        return false
+    }
+
+
+    override fun hashCode(): Int {
+        var result = x.hashCode()
+        result = 31 * result + y.hashCode()
+        return result
+    }
 
     /**
      * Draw the vector for debugging purposes.
@@ -66,64 +124,6 @@ class Vector2(x: Number, y: Number) {
         drawLine(color)
 
 
-    }
-
-    private constructor(both: Number) : this(both, both)
-    constructor() : this(0)
-
-    override fun toString() = "[$x, $y]"
-
-    operator fun plus(other: Vector2) = Vector2(x + other.x, y + other.y)
-    operator fun plus(other: Number) = plus(Vector2(other))
-
-    operator fun minus(other: Vector2) = Vector2(x - other.x, y - other.y)
-    operator fun minus(other: Number) = minus(Vector2(other))
-
-    operator fun times(other: Vector2) = Vector2(x * other.x, y * other.y)
-    operator fun times(other: Number) = times(Vector2(other))
-
-    operator fun div(other: Vector2) = Vector2(x / other.x, y / other.y)
-    operator fun div(other: Number) = div(Vector2(other))
-
-    operator fun unaryPlus() = this
-    operator fun unaryMinus() = this * -1
-
-    operator fun inc() = this + 1
-    operator fun dec() = this - 1
-
-    operator fun rem(other: Vector2) = Vector2(x % other.x, y % other.y)
-    operator fun rem(other: Number) = rem(Vector2(other))
-
-    fun lenSquared() = x * x + y * y
-    fun len() = sqrt(lenSquared())
-
-    infix fun longerThan(other: Vector2) = lenSquared() > other.lenSquared()
-    infix fun longerThan(other: Number) = len() > other.toDouble()
-    infix fun shorterThan(other: Vector2) = lenSquared() < other.lenSquared()
-    infix fun shorterThan(other: Number) = len() < other.toDouble()
-
-
-    fun dot(other: Vector2) = x * other.x + y * other.y
-    fun angle() = atan2(y, x)
-    fun angleTo(other: Vector2) = acos(dot(other) / len() * other.len())
-    fun angleToPoint(other: Vector2) = (other - this).angle()
-    fun normalized() = this / len()
-    fun rotatedBy(radians: Double) = fromAngle(angle() + radians, len())
-    fun rotatedAt(radians: Double) = fromAngle(radians, len())
-
-
-    override operator fun equals(other: Any?): Boolean {
-        if (other is Vector2) {
-            return (x == other.x) and (y == other.y)
-        }
-
-        return false
-    }
-
-    override fun hashCode(): Int {
-        var result = x.hashCode()
-        result = 31 * result + y.hashCode()
-        return result
     }
 
     companion object{
