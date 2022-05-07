d2c5b5d5eb4e10b069a497ed3d5198dc753fb014
Ivar Fatland <ivar.fatland@lyse.net>
2022-05-07 17:44:59 +0200

    Added centered image transformation

--- drawable/GameImage.kt
+++ drawable/GameImage.kt
@@ -2,6 +2,7 @@ package king_game_engine.drawable
 
 import king_game_engine.geometry.Vector2
 import java.awt.Graphics2D
+import java.awt.Point
 import java.awt.geom.AffineTransform
 import java.awt.image.BufferedImage
 import java.io.File
@@ -9,8 +10,7 @@ import java.io.IOException
 import javax.imageio.ImageIO
 
 class GameImage(filename: String){
-    private var img: BufferedImage? = null
-    private val affineTransform = AffineTransform()
+    private var img: BufferedImage
 
     init {
         try {
@@ -21,12 +21,37 @@ class GameImage(filename: String){
 
     }
 
-    fun draw(canvas: Graphics2D, position: Vector2 = Vector2()) {
-        canvas.drawImage(img, position.x.toInt(), position.y.toInt(), null)
+    fun draw(
+        canvas: Graphics2D,
+        position: Vector2 = Vector2(),
+        rotation: Double = 0.0,
+        shear: Vector2 = Vector2(),
+        scale: Vector2 = Vector2(1, 1),
+        anchor: Vector2 = Vector2(img.width, img.height) / 2
+    ) {
+        fun AffineTransform.transform(source: Vector2): Vector2 {
+            val src = Point(source.xInt, source.yInt)
+            val dst = Point()
+            transform(src, dst)
+            return Vector2(dst.x, dst.y)
+        }
+        fun AffineTransform.translate(amount: Vector2) {
+            translate(amount.x, amount.y)
+        }
+
+        val at = AffineTransform()
+        at.translate(position - anchor + anchor * (scale - 1))
+
+        at.rotate(rotation, anchor.x, anchor.y)
+        at.scale(scale.x, scale.y)
+        at.translate(anchor)
+        at.shear(shear.x, shear.y)
+        at.translate(-anchor)
+
+
+        canvas.drawImage(img, at, null)
     }
 
-    fun draw(canvas: Graphics2D, x: Number = 0, y: Number = 0) {
-        draw(canvas, Vector2(x, y))
-    }
+
 
 }
