1559df175ba11b643a392e9a747054eb56261be9
Ivar Fatland <ivar.fatland@lyse.net>
2022-05-08 20:11:39 +0200

    Updated documentation and added mousePosition member

--- core/King.kt
+++ core/King.kt
@@ -1,19 +1,33 @@
 package king_game_engine.core
 
+import king_game_engine.geometry.Vector2
 import java.awt.Graphics2D
 
 
 /**
  * The superclass of all King games.
+ * Goals:
+ *  - Only expose the bare minimum from swing that is needed or useful to make a 2D game.
+ *  - Let the user make the game without worrying about things like resizing the game view.
+ *  - Giving easy access to keyboard and mouse input reading.
  */
 abstract  class King (val screenWidth: Int, val screenHeight: Int, val fps: Int) {
     private var backend: Backend? = null
 
+    /**
+     * The current position of the mouse. Accounts for the current game-view scale and position.
+     * The value will be [[screenWidth], [screenHeight]] if the mouse in the bottom-right corner
+     * of the drawable section of the game panel.
+     * The value will be [[0.0, 0.0]] if the mouse is in the top-left corner of the drawable section
+     * of the game panel.
+     */
+    var mousePosition: Vector2 = Vector2()
+        private set
 
     /**
      * Called once per frame before drawing.
      * @param dt Number of seconds since last update call.
-     * Use this to get movement speeds independent of framerate.
+     * Use this to get movement speeds independent of the chosen framerate.
      */
     abstract fun update(dt: Double)
 
@@ -30,11 +44,13 @@ abstract  class King (val screenWidth: Int, val screenHeight: Int, val fps: Int)
     fun keyIsDown(keyCode: Int) = backend?.keyIsDown(keyCode) ?: throw gameNotRunning
     fun keyJustPressed(keyCode: Int) = backend?.keyJustPressed(keyCode) ?: throw gameNotRunning
     fun keyJustReleased(keyCode: Int) = backend?.keyJustReleased(keyCode) ?: throw gameNotRunning
+
     /**
      * Starts the game.
      */
     fun run(): King {
         backend = Backend(screenWidth, screenHeight, fps, this)
+        backend!!.mousePositionRead.connect { this.mousePosition = it }
         return this
     }
 }
